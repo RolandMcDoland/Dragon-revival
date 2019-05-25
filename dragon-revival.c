5c8a1aaf16a86af3e961322e0f3038b824ffcfcc
RolandMcDoland <miki.franek@gmail.com>
2019-05-25 22:26:57 +0200

    Root process done

--- dragon-revival.c
+++ dragon-revival.c
@@ -5,6 +5,7 @@
 #include <semaphore.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
 
 // Seperate the professions between available processes
 int *setup(size)
@@ -36,6 +37,9 @@ int *setup(size)
 int main(int argc, char **argv)
 {
     int size, tid;
+    MPI_Status status;
+
+    int *profession_array;
 
     MPI_Init(&argc, &argv);
 
@@ -46,7 +50,44 @@ int main(int argc, char **argv)
     {
         /* Pass size - 1 as available processes since 
         root is exclusively devoted to generating contracts */
-        int *profession_array = setup(size - 1);
+        profession_array = setup(size - 1);
+
+        // Notify all processes about the profession assignment
+        for (int i = 1; i < size; i++)
+        {
+            MPI_Ssend(&profession_array, 1, MPI_INT, i, 0, MPI_COMM_WORLD );
+        }
+
+        // A counter generating new contract number every time
+        int contract_counter = 0;
+
+        int seconds;
+        srand(time(NULL));
+
+        // Main loop- generate a contract at random
+        while(1)
+        {
+            // Wait random amount of time
+            seconds = rand() % 9 + 1;
+            sleep(seconds);
+
+            // Notify all processes about the profession assignment
+            for (int i = 1; i < size; i++)
+            {
+                MPI_Ssend(&contract_counter, 1, MPI_INT, i, 0, MPI_COMM_WORLD );
+            }
+
+            contract_counter++;
+        }
+    }
+    else
+    {
+        // Only receive for testing purposes for now
+        while(1)
+        {
+            MPI_Recv(&profession_array, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            printf(" Otrzymalem od %d\n", status.MPI_SOURCE);
+        }
     }
 
     MPI_Finalize();
